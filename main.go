9538fe6fd1c0e387408c386a8f24e10f82efbd20
Tim Zubov <timofey.zubov.05@gmail.com>
2024-09-20 20:29:22 +0300

    add simple http server with index page

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func index(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "It is an index Page\n")
+}
 
 func main() {
-	fmt.Println("Hello from Tim!")
+	http.HandleFunc("/", index)
+
+	http.ListenAndServe("localhost:8080", nil)
 }
