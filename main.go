323e756a360a51b11d8c93044db073a9ed05205d
Tim Zubov <timofey.zubov.05@gmail.com>
2024-09-21 00:00:11 +0300

    add gorilla/mux as router

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
@@ -9,8 +10,21 @@ func index(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "It is an index Page\n")
 }
 
-func main() {
-	http.HandleFunc("/", index)
+func getDataFromMap(w http.ResponseWriter, req *http.Request) {
+	var reqVars = mux.Vars(req)
 
+	fmt.Fprintf(w, "data-id: %s", reqVars["id"])
+}
+
+func getCustomNotFoundError(w http.ResponseWriter, req *http.Request) {
+	http.ServeFile(w, req, "static/404error.html")
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", index)
+	r.HandleFunc("/data/{id}", getDataFromMap)
+	r.PathPrefix("/").HandlerFunc(getCustomNotFoundError)
+	http.Handle("/", r)
 	http.ListenAndServe("localhost:8080", nil)
 }
